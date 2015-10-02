0f5f130378a6c8e0784e6e6e654d514189468c9d
lafolle <fghjklp@gmail.com>
2015-10-02 23:21:46 +0530

    init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	SERVE_ADDR = "localhost:7080"
+)
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path == "/" {
+		// serve home page
+		http.ServeFile(w, r, "home.html")
+	} else {
+		// serve blog if corresponding blog exists.
+		file_to_serve := fmt.Sprintf("blogs/%s.html", r.URL.Path[1:])
+		log.Println("Serving file", file_to_serve)
+		http.ServeFile(w, r, file_to_serve)
+
+		// other wise go redirect the user to home page
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/", mainHandler)
+	log.Println("Listening on", SERVE_ADDR)
+	log.Fatal(http.ListenAndServe(SERVE_ADDR, nil))
+
+}
